a9207036a78a5adde4700eb412f89149dfcb21e9
esaumorales <esaumorales2322@gmail.com>
2025-04-10 00:54:56 -0500

    header-footer-hero design

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
+import {
+  defineConfig
+} from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
+import {
+  fileURLToPath
+} from 'url'
 
+const __dirname = fileURLToPath(new URL('.',
+  import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,4 +17,14 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@assets/': path.resolve(__dirname, './src/assets'),
+      '@assets/*': path.resolve(__dirname, './src/assets/*'),
+      '@utils': path.resolve(__dirname, './src/utils'),
+    }
+  }
 })
