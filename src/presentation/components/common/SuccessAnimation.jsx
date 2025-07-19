17418231dde0acae6ba6ae9e977727cfdaf0ef44
esaumorales <esaumorales2322@gmail.com>
2025-07-18 20:15:26 -0500

    modal register fixed

--- src/presentation/components/common/SuccessAnimation.jsx
+++ src/presentation/components/common/SuccessAnimation.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { CheckCircle } from 'lucide-react';
+import { AiOutlineCheckCircle } from 'react-icons/ai';
 import PropTypes from 'prop-types';
 
 const SuccessAnimation = ({ message }) => {
     return (
         <div className="flex flex-col items-center justify-center py-12">
             <div className="bg-green-500 rounded-full p-4 mb-4 animate-bounce">
-                <CheckCircle className="text-white w-12 h-12" />
+                <AiOutlineCheckCircle className="text-white w-12 h-12" />
             </div>
             <p className="text-gray-600">{message}</p>
         </div>
